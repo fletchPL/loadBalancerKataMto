c81d9c8ca4e16b7497f647ceb27174378d7e1fce
Krystian Ryniec <187796@edu.p.lodz.pl>
2016-05-05 18:46:24 +0200

    Test1 resoult GREEN

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -4,6 +4,7 @@ package edu.iis.mto.serverloadbalancer;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
@@ -16,7 +17,42 @@ public class ServerLoadBalancerTest {
 	public void balancingServerWithNoVms_serverStayEmpty()
 	{
 		Server theServer = a(server().withCapacity(1));
+		
 		balance(aListOfServerWith(theServer), anEmptyListOfVms());
+		
 		assertThat(theServer, hasLoadPercentageOf(0.0d));
+	}
+
+
+
+	private Matcher<? super Server> hasLoadPercentageOf(double expectedloadPercentagle) {
+		// TODO Auto-generated method stub
+		return new CurrentLoadPercentageMatcher(expectedloadPercentagle);
+	}
+
+	private void balance(Server[] servers, Vm[] vms) {
+		// TODO Auto-generated method stub
+		new ServerLoadBalancer().balance(servers, vms);
+		
+	}
+
+	private Vm[] anEmptyListOfVms() {
+		// TODO Auto-generated method stub
+		return new Vm[0];
+	}
+
+	private Server[] aListOfServerWith(Server... servers) {
+		// TODO Auto-generated method stub
+		return servers;
+	}
+
+	private Server a(ServerBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.build();
+	}
+
+	private ServerBuilder server() {
+		// TODO Auto-generated method stub
+		return new ServerBuilder();
 	}
 }
